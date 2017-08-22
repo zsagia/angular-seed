d6d39549a846912e46131d6627dd4be3b5be326d
zsagia <zsagia@gmail.com>
2017-08-22 23:48:35 +0200

    2

--- app/app.component.ts
+++ app/app.component.ts
@@ -3,6 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'my-app',
   template: `
+  <ul class="menu">
+  <li><a href="#!/view1">view1</a></li>
+  <li><a href="#!/view2">view2</a></li>
+  <li><a href="/bar">bar</a></li>
+</ul>
     <router-outlet></router-outlet>
     <div ng-view></div>
   `,
